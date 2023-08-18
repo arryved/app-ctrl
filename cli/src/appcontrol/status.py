d89ce9cf740314c156051caad0512ef71eea31c5
Bill West <wwest@arryved.com>
2023-08-17 18:08:18 -0600

    Support different filtering options:
    - make application not required
    - add application column
    - add --canary-only flag

--- cli/src/appcontrol/status.py
+++ cli/src/appcontrol/status.py
@@ -19,12 +19,13 @@ api_hosts_by_env = {
 
 @click.command()
 @click.option('-e', '--environment', required=True)
-@click.option('-a', '--application', required=True)
-def status(environment, application):
+@click.option('-a', '--application', required=False, default="")
+@click.option('--canary-only', required=False, default=False, is_flag=True)
+def status(environment, application, canary_only):
     action = "status"
     api_host = api_hosts_by_env[environment]
     url = (f"{api_host}/{action}/{environment}/{application}")
-    click.echo(click.style(f"Connecting to {url}...", fg="green"))
+    click.echo(click.style(f"Connecting to {url} ...", fg="green"))
 
     with click_spinner.spinner():
         # TODO - use CA cert
@@ -36,7 +37,13 @@ def status(environment, application):
         click.echo(click.style(f"Server experienced an error: {decoded}", fg="red"), err=True)
         exit()
 
+    result = json.loads(response.text)
+    print_status_table(application, result, canary_only)
+
+
+def print_status_table(application, result, canary_only):
     table = ANSITable(
+        Column("Application", headstyle="bold"),
         Column("Host", headstyle="bold"),
         Column("Meta", headstyle="bold"),
         Column("Installed", headstyle="bold"),
@@ -46,30 +53,40 @@ def status(environment, application):
         border="thin"
     )
 
-    result = json.loads(response.text)
-    for host, status in result["hostStatuses"].items():
-        canary = "canary" if host in result["attributes"]["canaries"] else ""
-        meta = ','.join([canary])
-        installed = "<<red>>?"
-        running = "<<red>>?"
-        config = "<<red>>?"
-        healths = "?"
-        if status:
-            installed = render_version(status["versions"]["installed"])
-            running = render_version(status["versions"]["running"])
-            config = status["versions"]["config"]
-            config = config if config else "n/a"
-            healths = []
-            for h in status["health"]:
-                port = h["port"]
-                health = "OK" if h["healthy"] else "DOWN"
-                health = "?" if h["unknown"] else health
-                healths.append(f"{health}|{port}")    
-        healths = " ".join(healths)
-        color = "red" if "?" in healths or "DOWN" in healths else "green"
-        healths = f"<<{color}>>{healths}" if healths else "n/a"
-        table.row(host, meta, installed, running, config, healths)
+    if application == "":
+      results = result
+    else:
+      results = {application: result}
+
+    for application, result in results.items():
+        for host, status in result["hostStatuses"].items():
+            canary = "canary" if host in result["attributes"]["canaries"] else ""
+            meta = ','.join([canary])
+            installed = "<<red>>?"
+            running = "<<red>>?"
+            config = "<<red>>?"
+            healths = "?"
+            if status:
+                installed = render_version(status["versions"]["installed"])
+                running = render_version(status["versions"]["running"])
+                config = status["versions"]["config"]
+                config = config if config else "n/a"
+                healths = []
+                for h in status["health"]:
+                    port = h["port"]
+                    health = "OK" if h["healthy"] else "DOWN"
+                    health = "?" if h["unknown"] else health
+                    healths.append(f"{health}|{port}")    
+            healths = " ".join(healths)
+            color = "red" if "?" in healths or "DOWN" in healths else "green"
+            healths = f"<<{color}>>{healths}" if healths else "n/a"
+            if canary_only and not canary:
+              pass
+            else:
+              table.row(application, host, meta, installed, running, config, healths)
+
     table.print()
+
 
 def render_version(version):
     major = version["major"]
--- cli/src/setup.py
+++ cli/src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='app-control',
-    version='1.0.0',
+    version='2.0.0',
 
 	description='app-control CLI',
     long_description_content_type='text/markdown',
