d8236dd536e67cdd703f4c0333e5d4c5d7660f94
Bill West <wwest@arryved.com>
2024-05-22 08:29:11 -0600

    Add variant go deploy

--- cli/src/appcontrol/deploy.py
+++ cli/src/appcontrol/deploy.py
@@ -1,5 +1,6 @@
 import click
 import click_spinner
+import getpass
 import json
 import math
 import requests
@@ -10,21 +11,26 @@ from appcontrol.common import constants
 
 warnings.filterwarnings("ignore")
 
-
 @click.command()
 @click.option('-e', '--environment', required=True)
-@click.option('-a', '--application', required=True, default="")
-@click.option('-r', '--region', required=False, default="us-central1")
+@click.option('-a', '--application', required=True)
+@click.option('-r', '--region', required=False, default="central")
 @click.option('-t', '--variant', required=False, default="default")
-def deploy(environment, application, variant):
+@click.option('-n', '--version', required=True)
+def deploy(environment, application, region, variant, version):
     action = "deploy"
     api_host = constants["api_hosts_by_env"][environment]
-    url = (f"{api_host}/{action}/{environment}/{application}/{variant}")
+    url = (f"{api_host}/{action}/{environment}/{application}/{region}/{variant}")
     click.echo(click.style(f"Connecting to {url} ...", fg="green"))
 
     with click_spinner.spinner():
+        body = {
+                "principal": _principal(),
+                "concurrency": "1",
+                "version": version,
+        }
         # TODO - use CA cert
-        response = requests.get(url, verify=False)
+        response = requests.post(url, json=body, verify=False)
 
     status_code = math.floor(response.status_code / 100)
     if status_code == 5:
@@ -34,3 +40,9 @@ def deploy(environment, application, variant):
 
     result = json.loads(response.text)
     print(result)
+
+
+def _principal():
+    # TODO implement auth instead
+    username = getpass.getuser()
+    return username
